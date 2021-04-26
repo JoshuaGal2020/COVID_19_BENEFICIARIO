01303651117c12cc8e1625a0c8769fccd7f2b10b
JoshuaGal2020 <gg0210032019@unab.edu.sv>
2021-04-26 13:38:15 -0600

    Conexion

---
+++ ayuda/src/main/java/ayuda/joshua/Conexion/ConexionBd.java
@@ -0,0 +1,23 @@
+package ayuda.joshua.Conexion;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConexionBd {
+private Connection coonec;
+    
+    public ConexionBd()
+    {
+    try{Class.forName("com.mysql.jdbc.Driver");
+        coonec = DriverManager.getConnection("jdbc:mysql://localhost/covid","root","root");
+        System.out.println("CONECTADO A LA BD");
+        
+    }catch (Exception e)
+    {
+        System.out.println("ERROR DE CONEXION A LA BD "+ e);
+    }
+    }
+    public Connection RetornarConexion(){   
+        return coonec;   
+    }
+
+}
